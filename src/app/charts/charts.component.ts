401737963d7f1ce8f4416455b72828535119ebb7
Hexdee <lukmansaheed79@gmail.com>
2023-07-11 12:00:12 +0100

    Add chart page

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-charts',
@@ -6,5 +7,135 @@ import { Component } from '@angular/core';
   styleUrls: ['./charts.component.css']
 })
 export class ChartsComponent {
+
+  public scatterChartDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
+    {
+      data: [
+        { x: 1, y: 1 },
+        { x: 2, y: 3 },
+        { x: 3, y: -2 },
+        { x: 4, y: 4 },
+        { x: 5, y: -3 },
+      ],
+      label: 'Series A',
+      pointRadius: 10,
+    },
+  ];
+
+  public scatterChartOptions: ChartConfiguration<'scatter'>['options'] = {
+    responsive: true,
+  };
+
+  public bubbleChartOptions: ChartConfiguration<'bubble'>['options'] = {
+    responsive: true,
+    scales: {
+      x: {
+        min: 0,
+        max: 30,
+      },
+      y: {
+        min: 0,
+        max: 30,
+      }
+    }
+  };
+  public bubbleChartLegend = true;
+
+  public bubbleChartDatasets: ChartConfiguration<'bubble'>['data']['datasets'] = [
+    {
+      data: [
+        { x: 10, y: 10, r: 10 },
+        { x: 15, y: 5, r: 15 },
+        { x: 26, y: 12, r: 23 },
+        { x: 7, y: 8, r: 8 },
+      ],
+      label: 'Series A',
+    },
+  ];
+
+  // PolarArea
+  public polarAreaChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales', 'Telesales', 'Corporate Sales'];
+  public polarAreaChartDatasets: ChartConfiguration<'polarArea'>['data']['datasets'] = [
+    { data: [300, 500, 100, 40, 120] }
+  ];
+  public polarAreaLegend = true;
+
+  public polarAreaOptions: ChartConfiguration<'polarArea'>['options'] = {
+    responsive: true,
+  };
+
+  public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
+    responsive: true,
+  };
+  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartDatasets: ChartConfiguration<'radar'>['data']['datasets'] = [
+    { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
+  ];
+
+  // Doughnut
+  public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+    { data: [350, 450, 100], label: 'Series A' },
+    { data: [50, 150, 120], label: 'Series B' },
+    { data: [250, 130, 70], label: 'Series C' }
+  ];
+
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: true
+  };
+
+  public barChartLegend = true;
+  public barChartPlugins = [];
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ]
+  };
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+  };
+
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+  };
+  public pieChartLabels = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartDatasets = [{
+    data: [300, 500, 100]
+  }];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true
+  };
+  public lineChartLegend = true;
 
 }
